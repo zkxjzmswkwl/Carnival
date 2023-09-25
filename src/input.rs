d11a31f499f802deea984f5e37fd4d0acbf95c93
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-09-25 13:22:18 -0600

    actions,action structs, action chaining, parse actions from file

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,10 @@
+use std::{thread, time};
+
 use winput::{Button, Mouse};
 
 pub fn click(x: i32, y: i32) {
     if Mouse::set_position(x, y).is_ok() {
+        thread::sleep(time::Duration::from_millis(200));
         winput::send(Button::Left);
     }
 }
