b8271a0e75470fc54738b49a768e39b13013d1c1
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-09-27 22:21:23 -0600

    Return proper status codes for login/register responses

--- web/src/api/endpoints.rs
+++ web/src/api/endpoints.rs
@@ -1,8 +1,9 @@
+use easy_password::bcrypt::verify_password;
 use axum::{
     extract::State,
+    http::StatusCode,
     Json
 };
-use easy_password::bcrypt::verify_password;
 use crate::{
     api::payloads::{RegisterInput, LoginInput},
     CarnyState,
@@ -12,25 +13,28 @@ use crate::{
 pub async fn register(
     State(state): State<CarnyState>,
     Json(post_data): Json<RegisterInput>
-)-> String {
+)-> (StatusCode, String) {
 
     let username: &str = post_data.get_username();
     let password: &str = post_data.get_password();
     let password_conf: &str = post_data.get_password_conf();
 
     if password != password_conf {
-        return "Passwords do not match".to_string();
+        return (StatusCode::BAD_REQUEST,
+                "Passwords do not match".to_string());
     }
 
     if service::does_username_exist(username, &state.pool).await {
-        return "Username already exists".to_string();
+        return (StatusCode::BAD_REQUEST,
+                "Username already exists".to_string());
     }
 
     match service::create_user(username, password, &state.pool).await {
-        Ok(_) => return "Ok".to_string(),
+        Ok(_) => return (StatusCode::OK, "Created".to_string()),
         Err(e) => {
             eprintln!("{e}");
-            return "Error creating user.".to_string();
+            return (StatusCode::INTERNAL_SERVER_ERROR,
+                    "Error creating user.".to_string());
         }
     }
 }
@@ -38,22 +42,24 @@ pub async fn register(
 pub async fn login(
     State(state): State<CarnyState>,
     Json(post_data): Json<LoginInput>
-) -> String {
+) -> (StatusCode, String) {
 
     let username: &str = post_data.get_username();
     let password: &str = post_data.get_password();
 
     if !service::does_username_exist(username, &state.pool).await {
-        return "User does not exist".to_string();
+        return (StatusCode::BAD_REQUEST, "User does not exist".to_string());
     }
 
     let user = service::user_by_username(username, &state.pool).await;
     if !user.is_ok() {
-        return "Error fetching user".to_string();
+        return (StatusCode::INTERNAL_SERVER_ERROR,
+            "Error fetching user".to_string());
     }
 
     if verify_password(password, user.unwrap().get_password(), b"dev-VERYsecure").unwrap() {
-        return "totally-real-token".to_string();
+        return (StatusCode::OK, "totally-real-token".to_string());
     }
-    return "Incorrect username or password".to_string();
+
+    return (StatusCode::BAD_REQUEST, "Incorrect username or password".to_string());
 }
