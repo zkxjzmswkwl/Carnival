51ce181a771feb771903fe65392e7a6dd540fc18
vablings <redacted>
2023-09-28 22:17:36 +0100

    im far too tired to fix this right now, reverting

--- carnival-matchserver/src/overwatch/actions.rs
+++ carnival-matchserver/src/overwatch/actions.rs
@@ -1,51 +1,12 @@
 use crate::input;
 use serde::{Deserialize, Serialize};
+use toml::map::Map;
 use winput::Mouse;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::{thread, time};
 
-#[derive(Default, Serialize, Deserialize)]
-pub struct ActionChain(HashMap<String, Vec<Action>>);
 
-impl ActionChain {
-    //todo; make it worky
-}
-
-#[derive(Serialize, Deserialize)]
-enum Action { 
-    Mouse(MouseAction),
-    Keyboard(KeyboardAction)
-}
-
-impl Default for Action { 
-    fn default() -> Self {
-        Action::Mouse(MouseAction {
-            x: 0,
-            y: 0,
-            delay: 6969,
-        })
-    }
-}
-
-#[derive(Default, Serialize, Deserialize)]
-struct MouseAction { 
-    x: i32,
-    y: i32,
-    delay: u64,
-}
-
-#[derive(Default, Serialize, Deserialize)]
-struct KeyboardAction {
-    key: String,
-    delay: u64,
-}
-
-
-
-
-
-/* 
 #[allow(dead_code)]
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, Hash)] // TODO: Figure out why I need Copy, Clone and Hash? cbf
 struct Action {
@@ -62,7 +23,6 @@ pub struct Actions {
 impl Action {
     pub fn invoke(self) {
         log::debug!(" Invoking action: \n {:#?}", self);
-
         input::click(self.x, self.y);
         thread::sleep(time::Duration::from_millis(self.delay));
     }
@@ -95,4 +55,3 @@ impl Actions {
         }
     }
 }
-*/
