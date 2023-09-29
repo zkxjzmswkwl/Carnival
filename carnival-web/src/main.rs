d39109348ebeca60efdd0e58021a085e01316b05
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-09-29 01:13:59 -0600

    very shit ssr w/ htmx. super funny silly easter egg

--- carnival-web/src/main.rs
+++ carnival-web/src/main.rs
@@ -1,12 +1,15 @@
-use std::{net::SocketAddr, env};
-
 #[macro_use]
 extern crate dotenv_codegen;
 
+use std::{net::SocketAddr, env};
+use http::{Method, HeaderName};
+use rendering::components::{register_form, login_form, login_route, register_route};
+use tower::{ServiceBuilder, ServiceExt, Service};
+use tower_http::cors::{Any, CorsLayer};
 use axum::{
     routing::{get, post},
     Router,
-    Server
+    Server, http::header::CONTENT_TYPE
 };
 use api::endpoints::{
     register,
@@ -17,6 +20,7 @@ use crate::db::queries::tables;
 
 mod db;
 mod api;
+mod rendering;
 
 
 const HMAC_KEY: &[u8] = dotenv!("HMAC_KEY").as_bytes();
@@ -51,11 +55,24 @@ async fn main() {
         env::set_var("RUST_BACKTRACE", "1");
     }
     let state = CarnyState::new().await;
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any)
+        .allow_headers([
+           CONTENT_TYPE,
+           HeaderName::from_lowercase(b"hx-request").unwrap(),
+           HeaderName::from_lowercase(b"hx-current-url").unwrap(),
+        ]);
 
     let app: Router = Router::new()
         .route("/", get(root))
+        .route("/login", get(login_route))
+        .route("/register", get(register_route))
         .route("/api/register", post(register))
         .route("/api/login", post(login))
+        .route("/components/registration", get(register_form))
+        .route("/components/login", get(login_form))
+        .layer(cors)
         .with_state(state.clone());
 
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
