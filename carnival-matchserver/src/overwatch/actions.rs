3f5ed32c8082147722b14c6b4464b3129a2aa590
vablings <47798035+vablings@users.noreply.github.com>
2023-10-01 20:26:52 +0100

    cargo fix; cargo fmt

--- carnival-matchserver/src/overwatch/actions.rs
+++ carnival-matchserver/src/overwatch/actions.rs
@@ -1,10 +1,10 @@
 use crate::input;
 use serde::{Deserialize, Serialize};
-use winput::{Mouse, Input};
+
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_to_string;
-use std::{time, thread};
+use std::{thread, time};
 
 #[allow(dead_code)]
 #[derive(Serialize, Deserialize, Debug, Clone, Hash)]
@@ -13,11 +13,11 @@ enum ActionStyle {
     KeyboardAction(KeyboardAction),
 }
 
-//this seems bad 
+//this seems bad
 //bad as in not good but still performant
-impl ActionStyle { 
-    fn invoke(&self) { 
-        match &self  {
+impl ActionStyle {
+    fn invoke(&self) {
+        match &self {
             ActionStyle::KeyboardAction(action) => action.invoke(),
             ActionStyle::MouseAction(action) => action.invoke(),
         }
@@ -30,7 +30,7 @@ trait Action {
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, Hash)]
 struct MouseAction {
     x: i32,
-    y: i32, 
+    y: i32,
     delay: u64,
 }
 #[derive(Serialize, Deserialize, Debug, Clone, Hash)]
@@ -39,14 +39,14 @@ struct KeyboardAction {
     delay: u64,
 }
 
-impl Action for MouseAction { 
+impl Action for MouseAction {
     fn invoke(&self) {
         log::debug!(" Invoking MouseAction: \n {self:#?}");
         input::click(self.x, self.y);
         thread::sleep(time::Duration::from_millis(self.delay));
-    }   
+    }
 }
-impl Action for KeyboardAction { 
+impl Action for KeyboardAction {
     fn invoke(&self) {
         log::debug!("Invoking KeyboardAction: \n {self:#?}");
         winput::send_str(&self.input);
@@ -54,16 +54,15 @@ impl Action for KeyboardAction {
     }
 }
 
-
-
-
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct ActionChain(HashMap<String, Vec<ActionStyle>>);
 
 impl ActionChain {
     pub fn invoke_chain(&self, name: &str) -> &Self {
-        let chain = self.0.get(name)
-        .unwrap_or_else(|| panic!("No chain by the name of \"{name}\""));
+        let chain = self
+            .0
+            .get(name)
+            .unwrap_or_else(|| panic!("No chain by the name of \"{name}\""));
         let chain_len = chain.len();
         log::debug!("Chain \"{name}\" has length of {chain_len}");
 
@@ -74,12 +73,10 @@ impl ActionChain {
         self
     }
 
-    //its either use json or parse a custom toml structure. 
+    //its either use json or parse a custom toml structure.
     // KEEP IT SIMPLE STUPID:  https://www.youtube.com/watch?v=k0qmkQGqpM8
-    pub fn load(&mut self,) -> Result<&Self, Box<dyn Error>> {
+    pub fn load(&mut self) -> Result<&Self, Box<dyn Error>> {
         *self = serde_json::from_str(&read_to_string("action_chains.json")?)?;
         Ok(self)
     }
-
 }
-
