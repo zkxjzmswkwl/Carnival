0cab00b3a814080f2ae30e2f4a8cf21945ae0b31
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-10-01 02:10:11 -0600

    setup script /carnival-web/setup/database_inator.sh

--- carnival-web/src/api/endpoints.rs
+++ carnival-web/src/api/endpoints.rs
@@ -1,30 +1,29 @@
 use std::{net::SocketAddr, str::FromStr};
 
+use crate::db::services::session_token as session;
+use crate::{
+    api::payloads::{LoginInput, RegisterInput},
+    db::services::{queue, session_token::token_from_cookies, user},
+    rendering::components::{build_queue_comp, queue_table},
+    CarnyState, HMAC_KEY,
+};
+use axum::{
+    body::{Bytes, Full},
+    extract::{ConnectInfo, State},
+    response::Response,
+    Json, TypedHeader,
+};
 use easy_password::bcrypt::verify_password;
 use headers::Cookie;
+use http::{HeaderName, HeaderValue, StatusCode};
 use static_str_ops::static_format;
-use axum::{
-    extract::{State, ConnectInfo},
-    response::Response,
-    body::{Full, Bytes},
-    TypedHeader,
-    Json
-};
-use http::{StatusCode, HeaderValue, HeaderName};
-use crate::{
-    api::payloads::{RegisterInput, LoginInput},
-    CarnyState,
-    db::services::{user, queue, session_token::{token_from_cookies}}, HMAC_KEY, rendering::components::{queue_table, build_queue_comp}
-};
-use crate::db::services::session_token as session;
 
 use super::payloads::{JoinQueueInput, LeaveQueueInput};
 
 pub async fn register(
     State(state): State<CarnyState>,
-    Json(post_data): Json<RegisterInput>
-)-> (StatusCode, String) {
-
+    Json(post_data): Json<RegisterInput>,
+) -> (StatusCode, String) {
     // NOTE(aalhendi): is this needed?
     let username: &str = &post_data.username;
     let password: &str = &post_data.password;
@@ -32,25 +31,34 @@ pub async fn register(
     let battletag: &str = &post_data.battletag;
 
     if password != password_conf {
-        return (StatusCode::BAD_REQUEST,
-                "Passwords do not match".to_string());
+        return (
+            StatusCode::BAD_REQUEST,
+            "Passwords do not match".to_string(),
+        );
     }
 
     if user::does_battletag_exist(battletag, &state.pool).await {
-        return (StatusCode::BAD_REQUEST, "Battletag already exists".to_string())
+        return (
+            StatusCode::BAD_REQUEST,
+            "Battletag already exists".to_string(),
+        );
     }
 
     if user::does_username_exist(username, &state.pool).await {
-        return (StatusCode::BAD_REQUEST,
-                "Username already exists".to_string());
+        return (
+            StatusCode::BAD_REQUEST,
+            "Username already exists".to_string(),
+        );
     }
 
     match user::create_user(username, password, battletag, &state.pool).await {
         Ok(_) => (StatusCode::OK, "Created".to_string()),
         Err(e) => {
             eprintln!("{e}");
-            (StatusCode::INTERNAL_SERVER_ERROR,
-                    "Error creating user.".to_string())
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error creating user.".to_string(),
+            )
         }
     }
 }
@@ -59,9 +67,8 @@ pub async fn register(
 pub async fn login(
     ConnectInfo(connection): ConnectInfo<SocketAddr>,
     State(state): State<CarnyState>,
-    Json(post_data): Json<LoginInput>
+    Json(post_data): Json<LoginInput>,
 ) -> Response<Full<Bytes>> {
-
     let mut r: Response<Full<Bytes>> = Response::new(Full::from("nil"));
 
     let username: &str = &post_data.username;
@@ -71,7 +78,7 @@ pub async fn login(
     let user = match user_result {
         Ok(unwrapped_user) => unwrapped_user,
         // If there is no user by the username posted to us, error out.
-        Err(_) =>  {
+        Err(_) => {
             *r.status_mut() = StatusCode::BAD_REQUEST;
             *r.body_mut() = Full::from("User does not exist");
             return r;
@@ -80,8 +87,11 @@ pub async fn login(
 
     if verify_password(password, &user.password, HMAC_KEY).unwrap() {
         // checks to see if the requesting user has a valid token already.
-        let needs_token = session::token_by_user_id(user.id, &state.pool).await.is_none();
-        // If they don't, create one. 
+
+        let needs_token = session::token_by_user_id(user.id, &state.pool)
+            .await
+            .is_none();
+        // If they don't, create one.
         if needs_token {
             let session_option = session::create(&connection, user.id, &state.pool).await;
             // This would suck.
@@ -98,7 +108,7 @@ pub async fn login(
         //      If someone knows a user's password, they know their password.
         //      They will still be assigned a token (as of now).
         //
-        //      This *should* be tossed into middleware of some kind and then 
+        //      This *should* be tossed into middleware of some kind and then
         //      invoked prior to execution on any endpoint route that is denoted
         //      to require authorization.
         //
@@ -109,9 +119,10 @@ pub async fn login(
                 *r.body_mut() = Full::from("Nice");
                 r.headers_mut().insert(
                     "set-Cookie",
-                    HeaderValue::from_str(static_format!("session_id=Bearer {};path=/;", session)).unwrap()
+                    HeaderValue::from_str(static_format!("session_id=Bearer {};path=/;", session))
+                        .unwrap(),
                 );
-            },
+            }
             // If it has, we get very upset and tell the client that their actions are
             // "NOT_ACCEPTABLE".
             None => {
@@ -121,7 +132,7 @@ pub async fn login(
         }
         return r;
     }
-  
+
     // If we haven't dipped yet then this is the only remaining possibility.
     *r.status_mut() = StatusCode::BAD_REQUEST;
     *r.body_mut() = Full::from("Incorrect username or password");
@@ -131,20 +142,25 @@ pub async fn login(
 pub async fn join_queue(
     TypedHeader(cookies): TypedHeader<Cookie>,
     State(state): State<CarnyState>,
-    Json(post_data): Json<JoinQueueInput>
+    Json(post_data): Json<JoinQueueInput>,
 ) -> (StatusCode, String) {
-
     let queue_id_i32: i32 = post_data.queue_id.parse().unwrap_or_default();
     if let Some(requesting_user) = user::from_cookies(&cookies, &state.pool).await {
         if queue::add_user_to_queue(
             queue_id_i32,
             requesting_user.id,
-            &post_data.role, 
-            &state.pool
-        ).await.is_ok() {
+            &post_data.role,
+            &state.pool,
+        )
+        .await
+        .is_ok()
+        {
             // ? - I don't want to go through shit and make everything (StatusCode, Option<String>)
             // but maybe it's worth it? idk. don't want to. 🐒
-            return (StatusCode::CREATED, build_queue_comp(&cookies, &state.pool).await);
+            return (
+                StatusCode::CREATED,
+                build_queue_comp(&cookies, &state.pool).await,
+            );
         }
     }
     (StatusCode::OK, "asidjasid".to_string())
@@ -154,21 +170,19 @@ pub async fn join_queue(
 pub async fn leave_queue(
     TypedHeader(cookies): TypedHeader<Cookie>,
     State(state): State<CarnyState>,
-    Json(post_data): Json<LeaveQueueInput>
+    Json(post_data): Json<LeaveQueueInput>,
 ) -> (StatusCode, String) {
-
     // This has no error handling.
     // All that can be used to determine if something's gone wrong
     // here, is the fact that `queue::delete_user_from_queue` calls `eprintln!`.
     // You're welcome 😎
     let queue_id_i32: i32 = post_data.queue_id.parse().unwrap_or_default();
     if let Some(requesting_user) = user::from_cookies(&cookies, &state.pool).await {
-        queue::delete_user_from_queue(
-            queue_id_i32,
-            requesting_user.id,
-            &state.pool
-        ).await;
-        return (StatusCode::OK, build_queue_comp(&cookies, &state.pool).await);
+        queue::delete_user_from_queue(queue_id_i32, requesting_user.id, &state.pool).await;
+        return (
+            StatusCode::OK,
+            build_queue_comp(&cookies, &state.pool).await,
+        );
     }
     (StatusCode::OK, "Uhoh".to_string())
 }
--- carnival-web/src/db/services/session_token.rs
+++ carnival-web/src/db/services/session_token.rs
@@ -1,10 +1,10 @@
-use std::net::SocketAddr;
+use crate::db::models::SessionToken;
 use easy_password::bcrypt::{hash_password, verify_password};
 use headers::Cookie;
-use sqlx::{SqlitePool, sqlite::SqliteQueryResult};
-use crate::db::models::SessionToken;
-use rand::Rng;
 use rand::distributions::Alphanumeric;
+use rand::Rng;
+use sqlx::{sqlite::SqliteQueryResult, SqlitePool};
+use std::net::SocketAddr;
 
 fn rand_str() -> String {
     rand::thread_rng()
@@ -14,31 +14,40 @@ fn rand_str() -> String {
         .collect()
 }
 
-pub async fn create(
-    connection: &SocketAddr,
-    user_id: i32,
-    pool: &SqlitePool
-) -> Option<String> {
-
+pub async fn create(connection: &SocketAddr, user_id: i32, pool: &SqlitePool) -> Option<String> {
     // So apparently bcrypt has the concept of "cost". Which caps out at 31 or so.
     // Me personally, have no such concept on account of being extremely wealthy.
     // Doordash 20 times a week type money. You have no chance of getting it.
     // (This means we can't use a whole ass UUID as the hmac. Dang!)
-    let unique_hmac = rand_str();
-    // TODO: This can panic >_< 
-    let hashed_addr = hash_password(&connection.ip().to_string(), unique_hmac.as_bytes(), 12).unwrap();
+
+    let mut unique_hmac = String::from("fuey500");
+    if !cfg!(debug_assertions) {
+        unique_hmac = rand_str();
+    }
+
+    // TODO: This can panic >_<
+
+    // For generated test account (see database_inator.sh)
+    let mut remote_addr = String::from("127.0.0.1");
+    if !cfg!(debug_assertions) {
+        remote_addr = connection.ip().to_string();
+    }
+
+    let hashed_addr = hash_password(&remote_addr, unique_hmac.as_bytes(), 12).unwrap();
 
     // This looks shit. I think move **all** queries to queries.rs. That makes more sense anyway.
-    let insert_result = sqlx::query("INSERT INTO session_tokens 
+    let insert_result = sqlx::query(
+        "INSERT INTO session_tokens 
                 (for_user, remote_addr, unique_hmac_key, token, is_valid)
-                VALUES ($1, $2, $3, $4, $5);")
-     .bind(user_id)
-     .bind(connection.ip().to_string())
-     .bind(&unique_hmac)
-     .bind(&hashed_addr)
-     .bind(true)
-     .execute(pool)
-     .await;
+                VALUES ($1, $2, $3, $4, $5);",
+    )
+    .bind(user_id)
+    .bind(connection.ip().to_string())
+    .bind(&unique_hmac)
+    .bind(&hashed_addr)
+    .bind(true)
+    .execute(pool)
+    .await;
 
     match insert_result {
         Ok(_) => Some(hashed_addr),
@@ -49,15 +58,13 @@ pub async fn create(
     }
 }
 
-pub async fn token_by_user_id(
-    user_id: i32,
-    pool: &SqlitePool
-) -> Option<SessionToken> {
-
-    let token = sqlx::query_as::<_, SessionToken>("SELECT * FROM session_tokens WHERE for_user = $1 AND is_valid = 1")
-        .bind(user_id)
-        .fetch_one(pool)
-        .await;
+pub async fn token_by_user_id(user_id: i32, pool: &SqlitePool) -> Option<SessionToken> {
+    let token = sqlx::query_as::<_, SessionToken>(
+        "SELECT * FROM session_tokens WHERE for_user = $1 AND is_valid = 1",
+    )
+    .bind(user_id)
+    .fetch_one(pool)
+    .await;
 
     if token.is_ok() {
         return Some(token.unwrap());
@@ -67,9 +74,8 @@ pub async fn token_by_user_id(
 
 pub async fn delete_by_user_id(
     user_id: i32,
-    pool: &SqlitePool
+    pool: &SqlitePool,
 ) -> Result<SqliteQueryResult, sqlx::Error> {
-
     sqlx::query("DELETE from session_tokens WHERE for_user = $1")
         .bind(user_id)
         .execute(pool)
@@ -80,30 +86,32 @@ pub async fn delete_by_user_id(
 pub async fn set_invalid(
     user_id: i32,
     invalidation_source: &str,
-    pool: &SqlitePool
+    pool: &SqlitePool,
 ) -> Result<SqliteQueryResult, sqlx::Error> {
-    sqlx::query("UPDATE session_tokens SET is_valid = 0, invalidation_source = $1 WHERE for_user = $2;")
-        .bind(invalidation_source)
-        .bind(user_id)
-        .execute(pool)
-        .await
+    sqlx::query(
+        "UPDATE session_tokens SET is_valid = 0, invalidation_source = $1 WHERE for_user = $2;",
+    )
+    .bind(invalidation_source)
+    .bind(user_id)
+    .execute(pool)
+    .await
 }
 
-pub async fn validate(
-    connection: &SocketAddr,
-    user_id: i32,
-    pool: &SqlitePool
-) ->  Option<String> {
+pub async fn validate(connection: &SocketAddr, user_id: i32, pool: &SqlitePool) -> Option<String> {
     if let Some(token) = token_by_user_id(user_id, pool).await {
         // Get remote addr without port
-        if let Ok(remotes_match) = verify_password(&connection.ip().to_string(), &token.token, token.unique_hmac_key.as_bytes()) {
+        if let Ok(remotes_match) = verify_password(
+            &connection.ip().to_string(),
+            &token.token,
+            token.unique_hmac_key.as_bytes(),
+        ) {
             if !remotes_match {
                 // Someone's trying to use a session token bound to a different ip than their current.
                 // Invalidate the session token, forcing the user to reauth with their password.
                 let invalidate = set_invalid(user_id, &connection.ip().to_string(), pool).await;
                 match invalidate {
                     Ok(_) => println!("invalidation ok"),
-                    Err(e) => eprintln!("{e}")
+                    Err(e) => eprintln!("{e}"),
                 }
                 return None;
             }
@@ -113,10 +121,7 @@ pub async fn validate(
     None
 }
 
-pub fn token_from_cookies(
-    cookies: &Cookie
-) -> Option<String> {
-
+pub fn token_from_cookies(cookies: &Cookie) -> Option<String> {
     let session_option = cookies.get("session_id");
     if session_option.is_none() {
         return None;
