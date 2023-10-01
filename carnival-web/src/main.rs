05971ed33a1e86ed1d3a287ad859c03eafe3d1f1
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-09-30 19:57:39 -0600

    queue table now displays current user information

--- carnival-web/src/main.rs
+++ carnival-web/src/main.rs
@@ -18,7 +18,6 @@ use api::endpoints::{
 use sqlx::{SqlitePool, Sqlite, migrate::MigrateDatabase};
 use crate::db::queries::tables;
 use crate::db::services::overwatch_match::ResolvedOverwatchMatch;
-use crate::db::services::queue::ResolvedQueue;
 
 mod db;
 mod api;
@@ -66,16 +65,6 @@ impl CarnyState {
         let create_queued_players_result = sqlx::query(&tables::CREATE_QUEUED_PLAYERS).execute(&pool).await.unwrap();
         println!("Queued Players table creation -> {:?}", create_queued_players_result);
 
-        // create_map("Lijiang Tower", "KOTH", &pool).await;
-        // create_map("Ilios", "KOTH", &pool).await;
-        // create_map("Nepal", "KOTH", &pool).await;
-        // create_map("Busan", "KOTH", &pool).await;
-        // create_map("Antarctic Peninsula", "KOTH", &pool).await.map_err(|e| eprintln!("{e}"));
-
-        let m = ResolvedOverwatchMatch::from_id(1, &pool).await;
-        // let m = ResolvedQueue::from_id(1, &pool).await;
-        println!("{:#?}", m);
-
         Self { pool }
     }
 }
@@ -109,12 +98,7 @@ async fn main() {
         .route("/components/login", get(login_form))
         .route("/components/hero", get(hero))
         .route("/components/queue_table/:username", get(queue_table))
-        .route(
-            "/components/queue_user_table/:username",
-            get({
-                move |path| queue_user_panel(path)
-            })
-        )
+        .route("/components/queue_user_table/:username", get(queue_user_panel))
         // Endpoints
         .route("/api/register", post(register))
         .route("/api/login", post(login))
