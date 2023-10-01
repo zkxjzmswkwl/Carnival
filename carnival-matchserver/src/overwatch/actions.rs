d4b168f57404c2ea17417e9892619435239fa0df
vablings <47798035+vablings@users.noreply.github.com>
2023-10-01 13:09:11 +0100

    impl action chain (json sorry carter)

--- carnival-matchserver/src/overwatch/actions.rs
+++ carnival-matchserver/src/overwatch/actions.rs
@@ -2,9 +2,60 @@ use crate::input;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use std::time::Duration;
 use std::{thread, time};
 
 #[allow(dead_code)]
+
+trait Action {
+    fn invoke() {
+    }
+}
+
+
+enum ActionStyle {
+    MouseAction(MouseAction),
+    KeyboardAction(KeyboardAction),
+}
+struct MouseAction {
+    x: i32,
+    y: i32, 
+    delay: u64,
+}
+struct KeyboardAction {
+    keycode: String,
+    delay: u64,
+}
+
+struct ActionChain(HashMap<String, Vec<ActionType>>);
+
+impl ActionChain {
+    pub fn invoke_chain(&self, name: &str) -> &Self {
+        let chain = self.chains.get(name)
+        .unwrap_or_else(|| panic!("No chain by the name of \"{name}\""));
+        let chain_len = chain.len();
+        log::debug!("Chain \"{name}\" has length of {chain_len}");
+
+        chain.iter().for_each(|action| {
+            //action.invoke();
+        });
+        self
+    }
+
+    fn to_json(&self) { 
+        let json = serde_json::to_str(&self.0);
+    }
+    fn from_json(&mut self, action_chains: &str) -> Result<&Self, Box<dyn Error>> {
+        let json = serde_json::from_str(action_chains)?;
+        *self = json;
+        Ok(self)
+    }
+}
+
+
+
+
+
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, Hash)] // TODO: Figure out why I need Copy, Clone and Hash? cbf
 struct Action {
     x: i32,
