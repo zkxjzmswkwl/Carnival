a9c17e3161a37197df9b5f2665896897f7d99094
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-10-01 22:46:18 -0600

    Closes #41 - Also add domino animations to leaderboard/queue rows.

--- carnival-web/src/rendering/routes.rs
+++ carnival-web/src/rendering/routes.rs
@@ -1,42 +1,57 @@
-use axum::response::Html;
-use crate::DOMAIN;
+use axum::{response::Html, extract::State, TypedHeader};
+use headers::Cookie;
+use crate::{DOMAIN, CarnyState};
 use super::components::base;
 
-pub async fn index() -> Html<String> {
-    Html(base().await.replace(
+pub async fn index(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/hero" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN),
         )
     )
 }
 
-pub async fn login_route() -> Html<String> {
-    Html(base().await.replace(
+pub async fn login_route(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/login" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN),
         )
     )
 }
 
-pub async fn register_route() -> Html<String> {
-    Html(base().await.replace(
+pub async fn register_route(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/registration" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN),
         )
     )
 }
 
-pub async fn leaderboard_route() -> Html<String> {
-    Html(base().await.replace(
+pub async fn leaderboard_route(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/leaderboard" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN)
         )
     )
 }
 
-#[axum_macros::debug_handler]
-pub async fn queue_route() -> Html<String> {
-    Html(base().await.replace(
+pub async fn queue_route(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/queue_table" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN),
         )
@@ -44,8 +59,11 @@ pub async fn queue_route() -> Html<String> {
 }
 
 /// NOT IMPLEMENTED
-pub async fn profile_route() -> Html<String> {
-    Html(base().await.replace(
+pub async fn profile_route(
+    State(state): State<CarnyState>,
+    TypedHeader(cookies): TypedHeader<Cookie>,
+) -> Html<String> {
+    Html(base(&state.pool, &cookies).await.replace(
             "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
             &format!(r###"<div hx-get="{}/components/profile" hx-trigger="load" hx-target="#app""></div>"###, DOMAIN),
         )
